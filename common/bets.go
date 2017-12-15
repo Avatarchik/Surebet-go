c6185f2283dde8788608ef94dffa173c12646da5
marshygeek <lester0578@gmail.com>
2017-12-16 01:48:19 +0400

    Add bet format

--- common/bets.go
+++ common/bets.go
@@ -1,5 +1,36 @@
 package common
 
-type Bet struct {
-	Bookmaker map[string]map[string]int `json:"bookmaker"`
+type factor float32
+
+type condFactor struct {
+	Cond  float32
+	Value factor
 }
+
+type Bets struct {
+	Part  int
+	O1    []factor
+	OX    []factor
+	O2    []factor
+	O1X   []factor
+	O12   []factor
+	OX2   []factor
+	TO    []condFactor
+	TU    []condFactor
+	IndTO []condFactor
+	IndTU []condFactor
+	Hand1 []condFactor
+	Hand2 []condFactor
+}
+
+type Event struct {
+	Team1 string
+	Team2 string
+	Parts []Bets
+}
+
+type Sport map[string][]Event
+
+type Bookmaker map[string]Sport
+
+type AllEvents map[string]Bookmaker
