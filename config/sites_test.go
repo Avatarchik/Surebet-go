92be2f6c6d7ba2bdd128c3c511c0c3ddd5370d3e
korovkinand <korovkinandg@gmail.com>
2017-12-24 14:51:06 +0400

    Site's full name is now titled. Add testing for name functions

---
+++ config/sites_test.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"github.com/korovkinand/surebetSearch/common"
+	"testing"
+)
+
+var sites *common.SitesInfo
+var sitesList []*common.SiteInfo
+
+func init() {
+	sites = &Sites
+	sitesList = []*common.SiteInfo{
+		sites.Fonbet,
+		sites.Marat,
+		sites.Olimp,
+		sites.Posit,
+	}
+}
+
+func TestFullNames(t *testing.T) {
+	fullNames := []string{
+		"Fonbet",
+		"Marathonbet",
+		"Olimp",
+		"Positivebet",
+	}
+	for siteNum, fullName := range fullNames {
+		if fullName != sitesList[siteNum].FullName() {
+			t.Error("full name isn't equal with known result")
+		}
+	}
+}
+
+func TestNames(t *testing.T) {
+	names := []string{
+		"fonbet",
+		"marat",
+		"olimp",
+		"posit",
+	}
+	for siteNum, name := range names {
+		if name != sitesList[siteNum].Name() {
+			t.Error("name isn't equal with known result")
+		}
+	}
+}
